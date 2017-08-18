e3af1fa12cdd252dc99d3cc5c7f3b8ac99f54ea6
gproduct <gproduct1379@yahoo.com>
2017-08-18 15:05:21 +0200

    Adding some support

--- src/BQ2429x.py
+++ src/BQ2429x.py
@@ -38,5 +38,5 @@ class BQ2429x(object):
 		self._device = i2c.get_i2c_device(address, **kwargs)
 
 	def get_status(self):
-		#self._device.write8(BQ2429x_ADDR ,BQ2429x_STATUS_ADDR)
+		self._device.write8(BQ2429x_ADDR ,BQ2429x_STATUS_ADDR)
 		return self._device.readU8(BQ2429x_STATUS_ADDR)
