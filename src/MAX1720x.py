53ed2cbecc9bcc63227cbbc0ac2ee46b2e23519a
gproduct <gproduct1379@yahoo.com>
2017-08-18 14:49:59 +0200

    back to not raw

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -46,8 +46,8 @@ class MAX1720x(object):
 		return data*0.078125; 
 
 	def get_temperature(self):
-		self._device.write8(MAX1720X_COMMAND_ADDR, MAX1720X_TEMP_ADDR)
-		combined = self._device.readRaw8() | (self._device.readRaw8() << 8)
+		#self._device.write8(MAX1720X_COMMAND_ADDR, MAX1720X_TEMP_ADDR)
+		combined = self._device.readU8(MAX1720X_TEMP_ADDR) | (self._device.readU8(MAX1720X_TEMP_ADDR) << 8)
 		
 
 		temp = float(combined) / 256
