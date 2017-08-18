2fc0bcc4b43809e778e618a9eaa3fe1d4ff5f3d0
gproduct <gproduct1379@yahoo.com>
2017-08-18 10:07:13 +0200

    to str(float)

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import BMP280
 
 def main():
 	sensor = BMP280.BMP280()
-	print "Temperature: " 	+ sensor.read_temperature()
-	print "Pressure: " 		+ sensor.read_pressure()
+	print "Temperature: " 	+ str(sensor.read_temperature())
+	print "Pressure: " 		+ str(sensor.read_pressure())
 	time.sleep(1)
 
 if __name__ == '__main__':
