6990f263dc3ca245b4ec1cf0b522a7112af8afb0
gproduct <gproduct1379@yahoo.com>
2017-08-18 15:00:15 +0200

    trying the usb charger

--- src/main.py
+++ src/main.py
@@ -2,13 +2,11 @@ import time
 
 import BMP280
 import MAX1720x
+import BQ2429x
 
 def main():
-	sensor = MAX1720x.MAX1720x()
-
-	temp = sensor.get_temperature()
-	print temp
-	print temp * 256
+	sensor = BQ2429x.BQ2429x()
+	print sensor.get_status()
 
 	time.sleep(1)
 
