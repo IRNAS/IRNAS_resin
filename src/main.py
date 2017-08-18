4d776942b886c3a026d538ea4c6e096dcf086771
gproduct <gproduct1379@yahoo.com>
2017-08-18 08:41:04 +0200

    Modified to hello world

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+'''from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +6,8 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)'''
+
+if __name__ == '__main__':
+	print "Hello World!"
+
