dfb0e3ea26ce9f12f9d72fda8c16fd83c795b571
gproduct <gproduct1379@yahoo.com>
2017-08-18 11:45:55 +0200

    temp exp 5

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -46,6 +46,5 @@ class MAX1720x(object):
 		return data*0.078125; 
 
 	def get_temperature(self):
-		data = self._device.readU8(MAX1720X_TEMP_ADDR) 
-		data = (data) | (data << 8)
+		data = self._device.readS16(MAX1720X_TEMP_ADDR) 
 		return data/256; 
