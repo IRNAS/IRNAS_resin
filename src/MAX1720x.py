73246cc0b26b5863972804c70b2f6ed3cef04238
gproduct <gproduct1379@yahoo.com>
2017-08-18 12:25:34 +0200

    temp exp

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -48,5 +48,5 @@ class MAX1720x(object):
 	def get_temperature(self):
 		self._device.write8(MAX1720X_TEMP_ADDR)
 		combined = self._device.readU8(MAX1720X_TEMP_ADDR)|(self._device.readU8(MAX1720X_TEMP_ADDR)<<8)
-	    soc = combined
+		soc = combined
 		return soc/256
