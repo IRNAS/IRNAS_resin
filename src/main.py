3737dbcc306f76b09a28ef157b1472c082acefc5
gproduct <gproduct1379@yahoo.com>
2017-08-18 13:00:14 +0200

    temp exp

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ def main():
 	sensor = MAX1720x.MAX1720x()
 
 	temp =  sensor.get_temperature()
-	print "Returned: " + temp
-	print "/256: " + temp/256
-	print "combined" + hex(temp | (temp << 8))
+	print temp
+	print temp/256
+	print hex(temp | (temp << 8))
 
 	time.sleep(1)
 
