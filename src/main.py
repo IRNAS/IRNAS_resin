f13fa25c7e5379638dec164b8734fd399f797e24
gproduct <gproduct1379@yahoo.com>
2017-08-21 14:33:38 +0200

    Testing the bmp

--- src/main.py
+++ src/main.py
@@ -5,12 +5,9 @@ import MAX1720x
 import BQ2429x
 
 def main():
-	#sensor = BQ2429x.BQ2429x()
-	#print sensor.get_status()
-	sensor = MAX1720x.MAX1720x()
-	print "MAX1720 : cell voltage : " + str(sensor.get_cell_voltage())
-	#rint "MAX1720 : current : " + str(sensor.get_current()) 
-	#print "MAX1720 : temp : " + str(sensor.get_temperature())
+	sensor = BMP280.BMP280()
+
+	print sensor.read_temperature()
 
 	time.sleep(1)
 
