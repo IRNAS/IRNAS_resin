da9e7a85aaffd3ce831fc452c4fdd1bcb7bd9c08
gproduct <gproduct1379@yahoo.com>
2017-08-21 11:56:46 +0200

    Trying something, makes no sense tbh

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import BQ2429x
 def main():
 	#sensor = BQ2429x.BQ2429x()
 	#print sensor.get_status()
-
+	sensor = MAX1720x.MAX1720x()
 	print "MAX1720 : cell voltage : " + str(sensor.get_cell_voltage())
 	#rint "MAX1720 : current : " + str(sensor.get_current()) 
 	#print "MAX1720 : temp : " + str(sensor.get_temperature())
@@ -16,7 +16,6 @@ def main():
 
 if __name__ == '__main__':
 
-	sensor = MAX1720x.MAX1720x()
 
 	while 1:
 		main()
