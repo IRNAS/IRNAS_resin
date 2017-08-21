b923f13c3c22823b7470a6f6ff82d1bced9e9130
gproduct <gproduct1379@yahoo.com>
2017-08-21 10:56:35 +0200

    Adding the begin pointer

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -51,7 +51,7 @@ class MAX1720x(object):
 
 	# def get_cell_voltage(self) - gets a cell voltage (~3V)
 	def get_cell_voltage(self):
-		#self._device.writeRaw8(MAX1704X_VCELL_ADDR) 	# no need for this really, but we will keep it
+		self._device.writeRaw8(MAX1704X_VCELL_ADDR) 	# no need for this really, but we will keep it
 		try:
 			return((self._device.readU8(MAX1704X_VCELL_ADDR) << 4) + (self._device.readU8(MAX1704X_VCELL_ADDR) >> 4)) * 0.00125
 		except:
