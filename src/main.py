ec0a053a8f620f6c2db4f66c7def6cad56d7a2a5
gproduct <gproduct1379@yahoo.com>
2017-08-22 09:03:15 +0200

    Adding some fancy lining for the eye

--- src/main.py
+++ src/main.py
@@ -5,10 +5,11 @@ import MAX1720x
 import BQ2429x
 
 def main():
-
+	print "-----------------------------------------------"
 	print "MAX1720x : cell voltage : " + str(sensor_max.get_cell_voltage()) + "mV"
 	print "MAX1720x : current : " + str(sensor_max.get_current())
-	print "MAX1720x : temperature : " + str(sensor_max.get_temperature())
+	print "MAX1720x : temperature : " + str(sensor_max.get_temperature()) + "C"
+	print "-----------------------------------------------"
 	time.sleep(1)
 
 if __name__ == '__main__':
