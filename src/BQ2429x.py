768c228ffb3521f0f893eb9597ec174739fdcc28
gproduct <gproduct1379@yahoo.com>
2017-08-23 09:08:29 +0200

    Try returning binary?

--- src/BQ2429x.py
+++ src/BQ2429x.py
@@ -166,10 +166,11 @@ class BQ2429x(object):
 
 		try:
 							# reading the current value from the register
-			writing_value = str(thresh) + str(precharge) + str(c_v_l)
-			self._device.write8(BQ2429x_CHARGE_VOL_CTRL_ADDR, writing_value)
+			writing_value = "0b" + str(thresh) + str(precharge) + str(c_v_l)
+			return bin(writing_value)
+			'''self._device.write8(BQ2429x_CHARGE_VOL_CTRL_ADDR, writing_value)
 			current_value = self._device.readU8(BQ2429x_CHARGE_VOL_CTRL_ADDR)
-			return current_value
+			return current_value'''
 
 
 		except:																				# can't do the above 
