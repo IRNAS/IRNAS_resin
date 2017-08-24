4ab4288353938586addf505d6dcceec626bb2f14
gproduct <gproduct1379@yahoo.com>
2017-08-24 14:08:22 +0200

    typo

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -46,7 +46,7 @@ class MAX1720x(object):
 	def get_cell_voltage(self):
 		try:						
 			value 	= self._device.readU16(MAX1720X_VCELL_ADDR)					# get the value dependents on the cell nu,ber
-			return value * 0.00125 * 2													# to get actual voltage need to calculate
+			return value * 0.078125												# to get actual voltage need to calculate
 		except:
 			print "Couldn't connect to MAX1720"
 			return 0
