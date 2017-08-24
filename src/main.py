a62600bc0e9d2513cbda5efc55b8b18e0617538e
gproduct <gproduct1379@yahoo.com>
2017-08-24 11:46:45 +0200

    bit raw

--- src/main.py
+++ src/main.py
@@ -13,12 +13,12 @@ def main():
 	combined = (i2c.read_byte_data(i2c_addr, 0x09 + 1) | (i2c.read_byte_data(i2c_addr, 0x09)))
 	voltage = combined * 0.078125
 	print "Reading all raw: " + str(voltage)
-
+'''
 	combined = i2c.read_word_data(i2c_addr, 0x09)
 	voltage = combined * 0.078125
 	print "Reading 16bit (raw): " + str(voltage)
-'''
-	print "Reading with adatfruit driver:" + str(sensor_max.get_cell_voltage())
+
+	#print "Reading with adatfruit driver:" + str(sensor_max.get_cell_voltage())
 	#print "Reading with adatfruit current (signed): " + str(sensor_max.get_current())
 	#debug_main()
 	time.sleep(1)
