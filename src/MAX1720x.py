7df3370d11e654bc74245d083b6b2d0f5d034507
gproduct <gproduct1379@yahoo.com>
2017-08-25 12:32:12 +0200

    trying it all together

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -41,6 +41,8 @@ class MAX1720x(object):
 		try:
 			self._device = I2C.get_i2c_device(MAX1720X_I2CADDR)					# connect to device
 			self.i2c = smbus.SMBus(1)
+
+			enable_minmax_current()
 		except:
 			print "Couldn't connect to MAX1720 | I2C init"						# coudlnt connect to i2c unit
 
@@ -77,10 +79,13 @@ class MAX1720x(object):
 			hi = ((combined >> 8) & 0xff)
 			lo = ((combined >> 0) & 0xff)
 			return "hi: " + str(hi) + " lo: " + str(lo)'''
-			current_enable = self._device.readU16(0x1BA)
-			new_enable = current_enable | 0b0000000000010000
-			#self._device.writeU16(new_enable)
-			return bin(new_enable)
+			
+			combined = self._device.readU16(0x1C)
+			print "hex: " + str(bin(combined))
+			hi = ((combined >> 8) & 0xff)
+			lo = ((combined >> 0) & 0xff)
+			return "hi: " + str(hi) + " lo: " + str(lo)
+
 		except:
 			print "Couldn't connect to MAX1720"
 			return 0
@@ -143,3 +148,9 @@ class MAX1720x(object):
 		except:
 			print "Couldn't connect to MAX1720"
 			return 0
+
+	def enable_minmax_current():
+		print "Enabling minmax current"
+		current_enable = self._device.readU16(0x1BA)
+		new_enable = current_enable | 0b0000000000010000
+		self._device.writeU16(new_enable)
