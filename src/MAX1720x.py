3e2f407ea8d79b34921be3130e3ee2a30e7af513
gproduct <gproduct1379@yahoo.com>
2017-08-28 10:34:15 +0200

    raw debug

--- src/MAX1720x.py
+++ src/MAX1720x.py
@@ -88,6 +88,8 @@ class MAX1720x(object):
 			maximum_current = combined & 0xFF00
 			minium_current 	= combined & 0x00FF 
 
+			print "Raw maximum: " + str(maximum_current)
+
 			# .0004mV / Rsense resolution
 			# 0.0004V / 0.010ohm resolution
 			# 0.04 resolution
