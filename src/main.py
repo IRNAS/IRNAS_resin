3763b4a5a5098324518849d89acd6e6d1e063d19
gproduct <gproduct1379@yahoo.com>
2017-08-29 10:25:27 +0200

    a bit shifted

--- src/main.py
+++ src/main.py
@@ -41,18 +41,18 @@ def debug_it_all():
 	print "MAX1720x : TTF ----------------- : " + str(sensor_max.get_TTF())
 	print "MAX1720x : Battery ------------- : " + str(sensor_max.get_battery_absent())
 	print "" 
-	print "BQ2429x : status - VSYS -------- : " + str(sensor_bq.get_status(BQ2429x.VSYS_STAT))
-	print "BQ2429x : status - THERM_STAT -- : " + str(sensor_bq.get_status(BQ2429x.THERM_STAT))
-	print "BQ2429x : status - PG_STAT ----- : " + str(sensor_bq.get_status(BQ2429x.PG_STAT))
-	print "BQ2429x : status - DPM_STAT ---- : " + str(sensor_bq.get_status(BQ2429x.DPM_STAT))
-	print "BQ2429x : status - CHRG_STAT --- : " + str(sensor_bq.get_status(BQ2429x.CHRG_STAT))
-	print "BQ2429x : status - VBUS_STAT --- : " + str(sensor_bq.get_status(BQ2429x.VBUS_STAT))
+	print "BQ2429x : status - VSYS --------- : " + str(sensor_bq.get_status(BQ2429x.VSYS_STAT))
+	print "BQ2429x : status - THERM_STAT --- : " + str(sensor_bq.get_status(BQ2429x.THERM_STAT))
+	print "BQ2429x : status - PG_STAT ------ : " + str(sensor_bq.get_status(BQ2429x.PG_STAT))
+	print "BQ2429x : status - DPM_STAT ----- : " + str(sensor_bq.get_status(BQ2429x.DPM_STAT))
+	print "BQ2429x : status - CHRG_STAT ---- : " + str(sensor_bq.get_status(BQ2429x.CHRG_STAT))
+	print "BQ2429x : status - VBUS_STAT ---- : " + str(sensor_bq.get_status(BQ2429x.VBUS_STAT))
 	print ""
-	print "BQ2429x : fault - NTC_FAULT ---- : " + str(sensor_bq.get_faults(BQ2429x.NTC_FAULT))
-	print "BQ2429x : fault - BAT_FAULT ---- : " + str(sensor_bq.get_faults(BQ2429x.BAT_FAULT))
-	print "BQ2429x : fault - CHRG_FAULT --- : " + str(sensor_bq.get_faults(BQ2429x.CHRG_FAULT))
-	print "BQ2429x : fault - BOOST_FAULT -- : " + str(sensor_bq.get_faults(BQ2429x.BOOST_FAULT))
-	print "BQ2429x : fault - WATCHDOG_FAULT : " + str(sensor_bq.get_faults(BQ2429x.WATCHDOG_FAULT))
+	print "BQ2429x : fault - NTC_FAULT ----- : " + str(sensor_bq.get_faults(BQ2429x.NTC_FAULT))
+	print "BQ2429x : fault - BAT_FAULT ----- : " + str(sensor_bq.get_faults(BQ2429x.BAT_FAULT))
+	print "BQ2429x : fault - CHRG_FAULT ---- : " + str(sensor_bq.get_faults(BQ2429x.CHRG_FAULT))
+	print "BQ2429x : fault - BOOST_FAULT --- : " + str(sensor_bq.get_faults(BQ2429x.BOOST_FAULT))
+	print "BQ2429x : fault - WATCHDOG_FAULT  : " + str(sensor_bq.get_faults(BQ2429x.WATCHDOG_FAULT))
 	print ""
 
 if __name__ == '__main__':
